af2a4668ef470c5673f2c0564b51db8861dba6c8
elchata5 <elchata5@hotmail.com>
2022-09-30 17:59:14 -0300

    se cambio el menu de lugar y  el maps

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
         return(
         <div>
             <section className="google-map">
-                <iframe src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" className="map-iframe" style={{border: 0}} allowFullScreen loading="lazy" width="100%" height={400} />
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13089.231666212054!2d-57.9771973!3d-34.8987215!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcaf6d3e6d0b324f7!2sAluminio%20Italia!5e0!3m2!1ses-419!2sar!4v1664571223636!5m2!1ses-419!2sar" className="map-iframe" style={{border: 0}} allowFullScreen loading="lazy" width="100%" height={400} />
             </section>
             <footer className="site-footer">
             <div className="container">
@@ -13,7 +13,7 @@ class Footer extends React.Component {
                 <div className="col-12">
                   <h5 className="text-white">
                     <i className="bi-geo-alt-fill me-2" />
-                    State of Rio de Janeiro, Brazil
+                    Tolosa, La Plata
                   </h5>
                   <a href="mailto:info@company.com" className="custom-link mt-3 mb-5">
                     info@company.com
--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ AOS.init()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Menu />
     <App />
     <Footer />
   </React.StrictMode>
